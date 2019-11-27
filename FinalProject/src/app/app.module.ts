9150f70a8fd44726e614dba75edf2d0a8e25a081
fionnd11 <43648375+fionnd11@users.noreply.github.com>
2019-11-27 14:16:26 +0000

    Initial commit

---
+++ FinalProject/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {HttpClientModule} from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {
+MatInputModule,
+MatCardModule,
+MatButtonModule,
+MatToolbarModule,
+MatExpansionModule,
+MatNativeDateModule,
+MatDatepickerModule
+} from '@angular/material';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+import { EditComponent } from './edit/edit.component';
+import {FormsModule} from '@angular/forms';
+import { HomePageComponent } from './home-page/home-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent,
+    ReadComponent,
+    EditComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+MatButtonModule,
+MatToolbarModule,
+MatExpansionModule,
+MatInputModule,
+MatDatepickerModule,
+MatNativeDateModule,
+FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
